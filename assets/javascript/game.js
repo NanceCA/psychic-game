1ceeb32a770e589b8065979aaa8171c558f0c013
Hinojos <nhinojos@deloitte.com>
2019-03-31 16:13:23 -0700

    adding updates and working app

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,7 +24,7 @@ Reset here
 //declaring necessary variables
 var wins = 0;
 var losses = 0;
-var userGuess = [];
+var userChoice = []; //array to capture the user responses
 var guessesRemainding = 9;
 var computerSelection;
 
@@ -47,9 +47,68 @@ function game() {
     function checkAnswer() {
 
         document.onkeyup = function (event) {
-            var userGuess = (event).toLowerCase();
+            //using the keyCode property to return user selection 
+            var userGuess = String.fromCharCode(event.keyCode).toLowerCase(); //what the user inputs
+
             console.log(userGuess);
 
+            if (userChoice.indexOf(userGuess) >= 0) {
+                alert("Looks like you already guessed that!")
+            }
+
+            else if (!(alphabet.includes(userGuess))) {
+                alert("That was an invalid entry. Choose a letter from the alphabet.")
+
+            }
+
+            else if (userGuess === computerSelection) {
+                alert("You did it! YOU WIN!")
+                wins = wins + 1;
+                console.log("wins " + wins)
+                document.getElementById("wins").innerHTML = wins;
+
+                gameReset();
+            }
+
+            else {
+                guessesRemainding--;
+                document.getElementById("guesses-left").innerHTML = guessesRemainding; //display guesses left
+                userChoice.push(userGuess); //appending guesses to array
+
+                console.log("These are the user guesses so far " + userGuess);//test the recorded guesses
+
+                document.getElementById("your-guesses").innerHTML = userChoice;
+
+                console.log("The user has the following guesses left " + guessesRemainding)
+
+                outofGuesses();
+            }
+
         }
+    }
+
+    function outofGuesses() {
+        if (guessesRemainding == 0) {
+            alert("I'm sorry,but it looks like you've lost.")
+            losses = losses + 1;
+            document.getElementById("losses").innerHTML = losses;
+
+            gameReset();
+
+        }
+        else {
+            checkAnswer();
+        }
+    }
+
+    function gameReset() {
+        alert("Let's play again. We'll keep track of the scores.")
+        guessesRemainding = 9;
+        userChoice = [];
+        document.getElementById("guesses-left").innerHTML = guessesRemainding;
+        document.getElementById("your-guesses").innerHTML = userChoice;
+
+        game();
+
     }
 }
